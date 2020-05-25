ea20f02a6579bcb75e00c808bd62db9b72ba1dd0
ngtrhieu <ngtrhieu0011@gmail.com>
2020-05-25 18:40:23 +0800

    Finish CreateCodeCommit repository task

--- internals/scaffold/tasks/task_create_repository.js
+++ internals/scaffold/tasks/task_create_repository.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/order */
 /* eslint-disable import/no-extraneous-dependencies */
 const AWS = require('aws-sdk');
+const shell = require('shelljs');
 const Task = require('../task_runner/task');
 const Step = require('../task_runner/step');
 
@@ -8,42 +9,61 @@ const { name, description } = JSON.parse(
   require('fs').readFileSync('package.json'),
 );
 
-const task = new Task(
-  'Create CocdeCommit repository',
+const task = (() => {
+  let repositoryUrl = '';
 
-  new Step({
-    name: 'Check project name',
-    execute: () =>
-      new Promise((resolve, reject) => {
-        if (name === 'express-server-boiterplate') {
-          reject(
-            new Error(
-              'Please run `yarn init` to re-initialize your project with new name',
-            ),
+  return new Task(
+    'Create CodeCommit repository',
+
+    new Step({
+      name: 'Check project name',
+      execute: async () => {
+        if (name === 'api-express-server-boiterplate') {
+          throw new Error(
+            'Please run `yarn init` to re-initialize your project with new name',
           );
         }
-        resolve();
-      }),
-  }),
+      },
+    }),
 
-  new Step({
-    name: 'Create repository',
-    execute: () => {
-      const client = new AWS.CodeCommit();
-      return client
-        .createRepository({
+    new Step({
+      name: 'Create repository',
+      execute: async () => {
+        const client = new AWS.CodeCommit();
+        const response = await client
+          .createRepository({
+            repositoryName: name,
+            repositoryDescription: description,
+          })
+          .promise();
+        repositoryUrl = response.repositoryMetadata.cloneUrlSsh;
+      },
+      rollback: () => {
+        const client = new AWS.CodeCommit();
+        return client.deleteRepository({
           repositoryName: name,
-          repositoryDescription: description,
-        })
-        .promise();
-    },
-    rollback: () => {
-      const client = new AWS.CodeCommit();
-      return client.deleteRepository({
-        repositoryName: name,
-      });
-    },
-  }),
-);
+        });
+      },
+    }),
+
+    new Step({
+      name: 'Is Git available?',
+      execute: async () => {
+        if (!shell.which('git')) {
+          throw new Error('git cannot be found.');
+        }
+      },
+    }),
+
+    new Step({
+      name: 'Push code to new repo',
+      execute: async () => {
+        shell.exec('git remote remove origin');
+        shell.exec(`git remote add origin ${repositoryUrl}`);
+        shell.exec('git push -u origin master');
+      },
+    }),
+  );
+})();
 
 module.exports = task;
